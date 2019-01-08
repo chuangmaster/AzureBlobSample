aa4ccdab1289e6c59c6d59522dcc643ebd3f4c50
chuangmaster <masterchuang@urtody.com>
2019-01-08 16:27:15 +0800

    加入Upload使用實例

--- AzureBlobSample/Program.cs
+++ AzureBlobSample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +18,14 @@ namespace AzureBlobSample
 
             //MoveBlobFile
             //預期從ContainerName/Directory1/2018122001/03/1-1.jpg 移動到 ContainerName/Directory2/1-1.jpg
-            service.MoveBlobFile("ContainerName","Directory1/2018122001/03/1-1.jpg", "Directory2/1-1.jpg");
+            service.MoveBlobFile("ContainerName", "Directory1/2018122001/03/1-1.jpg", "Directory2/1-1.jpg");
 
-
+            //Upload
+            FileStream fs = new FileStream(@"C:\Image.jpg", FileMode.Open);
+            byte[] bytes = new byte[fs.Length];
+            fs.Read(bytes, 0, bytes.Length);
+            Stream stream = new MemoryStream(bytes);
+            service.Upload(stream, "ContainerName", "Directory1/2018122001/03/1-1.jpg", ".jpg");
 
             Console.Read();
         }
